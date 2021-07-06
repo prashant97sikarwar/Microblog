cec3072bc846786310482432d25ac4b7f2b613e5
prashant sikarwar <prashant97sikarwar@gmail.com>
2021-07-07 01:23:09 +0530

    heroku deployment changes

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
@@ -13,7 +16,8 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['prashant97.official@gmail.com']
-    POSTS_PER_PAGE = 7
-    LANGUAGES = ['en','es']
+    LANGUAGES = ['en', 'es']
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
-    
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    POSTS_PER_PAGE = 7
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
